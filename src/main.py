fa7c62a897fb3e73f374562739b20e390fcd0301
Lili27 <66214597+Lili27@users.noreply.github.com>
2021-06-02 15:26:02 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -74,113 +74,118 @@ def get_args():
 
 
 def get_pre_parsing(pdbH):
-	"""
-	This function takes a pdb file which contain the hydrogen atoms given by Reduce.
-	The coordinates of the atoms involved in the peptide bond and carbon skeleton are only taken.
-	It is written in a pdb file.
-	Returns a list of dictionary which have these coordinates and the name of pdb file.
-	"""
-	pdbH_p = str(pdbH).split(".")[0] + "_preparsing." + str(pdbH).split(".")[1]
-	with open(pdbH, "r") as filin, open(pdbH_p, "w") as filout:
-		list_coord = []
-		i = 0 
-		line = filin.readlines()
-		while i < len(line):
-			dict_coord = {}
-			if line[i].startswith("ATOM") and (line[i][12:16].strip() == "N" or line[i][12:16].strip() == "C" or line[i][12:16].strip() == "O" or line[i][12:16].strip() == "H" or line[i][12:16].strip() == "CA") :
-				filout.write(line[i])
-				dict_coord["num_atom"] = str(line[i][6:11].strip())
-				dict_coord["atom_name"] = str(line[i][12:16].strip())
-				dict_coord["residu_name"] = str(line[i][17:20].strip())
-				dict_coord["chain"] = str(line[i][21:22].strip())
-				dict_coord["residu"] = int(line[i][22:26].strip())
-				dict_coord["x"] = float(line[i][30:38].strip())
-				dict_coord["y"] = float(line[i][38:46].strip())
-				dict_coord["z"] = float(line[i][46:54].strip())
-				list_coord.append(dict_coord)
-			elif (line[i].startswith("HETATM") or line[i].startswith("ATOM")) and (line[i][12:16].strip() != "N" or line[i][12:16].strip() != "C" or line[i][12:16].strip() != "O" or line[i][12:16].strip() != "H" or line[i][12:16].strip() != "CA") :
-				pass
-			else:
-				filout.write(line[i])
-			i += 1
-		return pdbH_p, list_coord
+    """
+    This function takes a pdb file which contain the hydrogen atoms given by Reduce.
+    The coordinates of the atoms involved in the peptide bond and carbon skeleton are only taken.
+    It is written in a pdb file.
+    Returns a list of dictionary which have these coordinates and the name of pdb file.
+    """
+    pdbH_p = str(pdbH).split(".")[0] + "_preparsing." + str(pdbH).split(".")[1]
+    with open(pdbH, "r") as filin, open(pdbH_p, "w") as filout:
+        list_coord = []
+        i = 0 
+        line = filin.readlines()
+        while i < len(line):
+            dict_coord = {}
+            if line[i].startswith("ATOM") and (line[i][12:16].strip() == "N" or line[i][12:16].strip() == "C" or line[i][12:16].strip() == "O" or line[i][12:16].strip() == "H" or line[i][12:16].strip() == "CA") :
+                filout.write(line[i])
+                dict_coord["num_atom"] = str(line[i][6:11].strip())
+                dict_coord["atom_name"] = str(line[i][12:16].strip())
+                dict_coord["residu_name"] = str(line[i][17:20].strip())
+                dict_coord["chain"] = str(line[i][21:22].strip())
+                dict_coord["residu"] = int(line[i][22:26].strip())
+                dict_coord["x"] = float(line[i][30:38].strip())
+                dict_coord["y"] = float(line[i][38:46].strip())
+                dict_coord["z"] = float(line[i][46:54].strip())
+                list_coord.append(dict_coord)
+            elif (line[i].startswith("HETATM") or line[i].startswith("ATOM")) and (line[i][12:16].strip() != "N" or line[i][12:16].strip() != "C" or line[i][12:16].strip() != "O" or line[i][12:16].strip() != "H" or line[i][12:16].strip() != "CA") :
+                pass
+            else:
+                filout.write(line[i])
+            i += 1
+        return pdbH_p, list_coord
 
 
 def get_count_residu(liste):
-	"""
-	This function takes a list of dictionary
-	Count the number of times a residue is present in the list
-	return a dictionary (str,int)
-	"""
-	i = 0
-	dico_count = {}
-	for i in range(len(liste)) :
-		if (str(liste[i]["residu"])) not in dico_count :
-			dico_count[str(liste[i]["residu"])] = 1
-		else :
-			dico_count[str(liste[i]["residu"])] += 1
-		i += 1
-	return dico_count
+    """
+    This function takes a list of dictionary
+    Count the number of times a residue is present in the list
+    return a dictionary (str,int)
+    """
+    i = 0
+    dico_count = {}
+    for i in range(len(liste)) :
+        if (str(liste[i]["residu"])) not in dico_count :
+            dico_count[str(liste[i]["residu"])] = 1
+        else :
+            dico_count[str(liste[i]["residu"])] += 1
+        i += 1
+    return dico_count
 
 
 def get_deleted(count_residu):
-	"""
-	This function takes a dictionary
-	A residu must have at least the 5 atoms (N,CA,C,O,H)
-	return a list of integer
-	"""
-	list_del = []
-	for NB in count_residu:
-		if count_residu[NB] < 5:
-			if NB not in list_del :
-				list_del.append(NB)
-	return list_del
+    """
+    This function takes a dictionary
+    A residu must have at least the 5 atoms (N,CA,C,O,H)
+    return a list of integer
+    """
+    list_del = []
+    for NB in count_residu:
+        if count_residu[NB] < 5:
+            if NB not in list_del :
+                list_del.append(NB)
+    return list_del
 
 
 def get_parsing(pre_parsing, residu_del):
-	"""
-	This functions takes 2 arguments
-	=> pre_parsing : a pdb file 
-	=> residu_del : list of integer
+    """
+    This functions takes 2 arguments
+    => pre_parsing : a pdb file 
+    => residu_del : list of integer
     Do the parsing
-	return the name of a pdb file after the parsing
-	"""
-	parsing = str(pre_parsing).split("_")[0] + "_parsing." + str(pre_parsing).split(".")[1]
-	with open(pre_parsing, "r") as filin, open(parsing, "w") as filout:
-		i = 0 
-		line = filin.readlines()
-		while i < len(line):
-			if (line[i][22:28].strip() not in residu_del):
-				filout.write(line[i])
-			i +=1
-		return parsing
+    return the name of a pdb file after the parsing
+    """
+    parsing = str(pre_parsing).split("_")[0] + "_parsing." + str(pre_parsing).split(".")[1]
+    with open(pre_parsing, "r") as filin, open(parsing, "w") as filout:
+        i = 0 
+        line = filin.readlines()
+        while i < len(line):
+            if (line[i][22:28].strip() not in residu_del):
+                filout.write(line[i])
+            i +=1
+        return parsing
 
 
 def get_list(list_pre_parsing, residu_del):
-	"""
-	This function takes 2 arguments:
-	=> list_pre_parsing : list of dictionary
-	=> residu_del : list of integer
+    """
+    This function takes 2 arguments:
+    => list_pre_parsing : list of dictionary
+    => residu_del : list of integer
     Give the coordinates (informations) of the atoms N,CA,C,O,H
-	return a list of dictionary
-	"""
-	list_parsing = []
-	for i in range(len(list_pre_parsing)):
-		if str(list_pre_parsing[i]["residu"]) in residu_del:
-			pass
-		else:
-			list_parsing.append(list_pre_parsing[i])
-		i += 1
-	return list_parsing
+    return a list of dictionary
+    """
+    list_parsing = []
+    for i in range(len(list_pre_parsing)):
+        if str(list_pre_parsing[i]["residu"]) in residu_del:
+            pass
+        else:
+            list_parsing.append(list_pre_parsing[i])
+        i += 1
+    return list_parsing
 
 
 def main():
     #arguments=get_args()
     #print(arguments)
-    
+    # ---------------------------------------
+    # STEPS FOR ARGUMENTS 
+    # ---------------------------------------
     # Verify the existence of arguments given in the python command
     args = verify_args(sys.argv)
     print(type(args))
+    # ---------------------------------------
+    # STEPS FOR REDUCE 
+    # ---------------------------------------
     # Produce the pdb file which contain the hydrogens atoms added by the Reduce program
     pdbH=pdbReduce(args)
     print(pdbH)
